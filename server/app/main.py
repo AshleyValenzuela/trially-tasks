81f77ff78894da9e8df2ea1ef78b6dbf730f9a68
ashley <ashleyvalenzuelaq@gmail.com>
2024-07-31 13:38:36 -0400

    adds config updates to allow for requests and docker network setup

--- server/app/main.py
+++ server/app/main.py
@@ -1,17 +1,31 @@
 from fastapi import FastAPI, Depends, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 import crud as crud
 import schemas as schemas
 import models as models
 from database import engine
 from routers import tasks_router, users_router
+import os
 
-# Create all database tables
+# Creates all database tables
 models.Base.metadata.create_all(bind=engine)
 
+# Initializes FastAPI app
 app = FastAPI()
 
-# Include the routers
+origins = os.getenv('CORS_ORIGINS', 'http://localhost:3000').split(',')
+
+# Adds CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Includes the routers
 app.include_router(tasks_router, prefix="/tasks", tags=["tasks"])
 app.include_router(users_router, prefix="/users", tags=["users"])
 
