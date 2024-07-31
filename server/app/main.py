b43373b0338c7056e314e8fea7dc3bf944e260e6
ashley <ashleyvalenzuelaq@gmail.com>
2024-07-30 21:15:58 -0400

    run fastapi app with uvicorn on main.py

--- server/app/main.py
+++ server/app/main.py
@@ -1,10 +1,9 @@
-import logging
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 import crud as crud
 import schemas as schemas
 import models as models
-from database import SessionLocal, engine
+from database import engine
 from routers import tasks_router, users_router
 
 # Create all database tables
@@ -12,23 +11,16 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-# Dependency to get a database session
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
 # Include the routers
 app.include_router(tasks_router, prefix="/tasks", tags=["tasks"])
 app.include_router(users_router, prefix="/users", tags=["users"])
 
+
 @app.get("/")
 def read_root():
-    logger.info("Root endpoint called")
     return {"Hello": "World"}
 
-import time
-while True:
-    time.sleep(100)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
