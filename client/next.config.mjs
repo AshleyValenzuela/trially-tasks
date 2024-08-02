4f15320ec9bf805e19c85c252b258684835061b4
ashley <ashleyvalenzuelaq@gmail.com>
2024-08-02 00:44:44 -0400

    account for server side and client side rendering in frontend api calls by making two api urls

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+        NEXT_PUBLIC_API_URL_SERVER_SIDE: process.env.NEXT_PUBLIC_API_URL_SERVER_SIDE,
+        NEXT_PUBLIC_API_URL_CLIENT_SIDE: process.env.NEXT_PUBLIC_API_URL_CLIENT_SIDE,
     },
 };
 
