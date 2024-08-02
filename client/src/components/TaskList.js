37b7e99be23fd59efeeca777c5ceb67fa92a155e
AshleyValenzuela <ashleyvalenzuelaq@gmail.com>
2024-08-02 11:57:56 -0400

    fix references to task.task_id to task.id

--- client/src/components/TaskList.js
+++ client/src/components/TaskList.js
@@ -1,14 +1,14 @@
 const TaskList = ({ tasks, onDelete, onUpdate }) => (
   <ul>
     {tasks.map((task) => (
-      <li key={task.task_id}>
+      <li key={task.id}>
         <div>
           <h3>{task.title}</h3>
           <p>{task.description}</p>
           <p>{task.status}</p>
           <p>{task.due_date}</p>
           <button onClick={() => onUpdate(task)}>Edit</button>
-          <button onClick={() => onDelete(task.task_id)}>Delete</button>
+          <button onClick={() => onDelete(task.id)}>Delete</button>
         </div>
       </li>
     ))}
